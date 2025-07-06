a69d07eb31e6659584e66c457e76b557d10d2bdb
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-06 18:46:14 +0200

    FEAT: create LEFT JOIN query to get properties with or without ratings

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,4 +6,7 @@ WHERE b.user_id == u.user_id;
 -- LEFT JOIN
 SELECT * FROM property p
 LEFT JOIN reviews r
-WHERE p.review_id == p.review_id;
+WHERE p.review_id == p.review_id
+ORDER BY r.rating;
+
+
