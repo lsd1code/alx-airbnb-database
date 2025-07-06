ea4b3f05d52c0a18e8ca66b27a8cc00752d33753
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-06 19:17:43 +0200

    FEAT: refactoring complex queries to improve performance.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,9 +1,11 @@
 SELECT
-      b.*,
-      u.*,
-      p.*,
-      pay.*
+    b.*,
+    u.*,
+    p.*,
+    pay.*
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id
 INNER JOIN properties p ON b.property_id = p.id
-LEFT JOIN payments pay ON b.id = pay.booking_id;
+LEFT JOIN payments pay ON b.id = pay.booking_id
+WHERE u.status = 'active'
+  AND p.city = 'New York';
