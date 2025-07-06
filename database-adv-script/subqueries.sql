068a7c5c587d1a3d950331415e3481da8346c76c
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-06 18:54:28 +0200

    FEAT: create correlated subquery to find users who have made more than 3 bookings

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -6,3 +6,11 @@ WHERE id IN (
     GROUP BY property_id
     HAVING AVG(rating) > 4.0
 );
+
+-- Correlated subquery
+SELECT * FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.id
+) > 3;
