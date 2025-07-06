4b87a8d0a1e7f55d67e4abe8b57188bb87f0ff0f
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-06 18:48:48 +0200

    FEAT: create FULL OUTER JOIN query to get all users and bookings

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,4 +9,7 @@ LEFT JOIN reviews r
 WHERE p.review_id == p.review_id
 ORDER BY r.rating;
 
-
+-- FULL OUTER JOIN
+SELECT * FROM users s
+FULL OUTER JOIN bookings b
+WHERE s.booking_id == b.booking_id;
